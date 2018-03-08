43df38371e8dc176dbdbd607ea7444a5b73f1c5a
YeHuaRen <33653511+YeHuaRen@users.noreply.github.com>
2018-03-08 14:25:13 +0800

    MKMKMKMKKM

---
+++ src/main/java/com/demo/MMM.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+/**
+ * Created by YeHuaRen on 2018/3/8 14:24.
+ */
+public class MMM {
+}
--- src/main/java/com/demo/PLMN.java
+++
@@ -1,7 +0,0 @@
-package com.demo;
-
-/**
- * Created by YeHuaRen on 2018/3/8 13:31.
- */
-public class PLMN {
-}
